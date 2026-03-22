eaba23079d0027d27a6774561dfa88989ef09576
Harshal Darak <harshalddarak09@gmail.com>
2026-03-22 13:15:10 +0530

    Add tanstack/react-query and set up QueryClientProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
-import 'bootstrap/dist/css/bootstrap.css';
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import "bootstrap/dist/css/bootstrap.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-).render(
+const quesryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
+    <QueryClientProvider client={quesryClient}>
+      <App />
+    </QueryClientProvider>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
