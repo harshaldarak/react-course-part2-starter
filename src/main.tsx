3e4baceff96fa26e1770e9a85ff442d7cca5c9fc
Harshal Darak <harshalddarak09@gmail.com>
2026-03-23 11:04:03 +0530

    refactor : Added Error and update Main file

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-const quesryClient = new QueryClient();
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <QueryClientProvider client={quesryClient}>
+    <QueryClientProvider client={queryClient}>
       <App />
     </QueryClientProvider>
-    <App />
   </React.StrictMode>,
 );
