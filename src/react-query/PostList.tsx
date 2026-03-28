66c8f528668d05278a4e27a1066f89726db416f5
Harshal Darak <harshalddarak09@gmail.com>
2026-03-28 17:15:43 +0530

    feat: Add pagination to fetch posts

--- src/react-query/PostList.tsx
+++ src/react-query/PostList.tsx
@@ -2,22 +2,13 @@ import { useState } from "react";
 import usePosts from "./hooks/usePosts";
 
 const PostList = () => {
-  const [userId, setUserId] = useState<number>();
-  const { data: posts, isLoading, error } = usePosts(userId);
+  const pageSize = 10;
+  const [page, setPage] = useState<number>(1);
+  const { data: posts, isLoading, error } = usePosts({ page, pageSize });
   if (isLoading) return <p>Loading ...</p>;
   if (error) return <p>{error.message}</p>;
   return (
     <>
-      <select
-        className='form-select mb-3'
-        onChange={(e) => setUserId(Number(e.target.value))}
-        value={userId}
-      >
-        <option value=''></option>
-        <option value='1'>User 1</option>
-        <option value='2'>User 2</option>
-        <option value='3'>User 3</option>
-      </select>
       <ul className='list-group'>
         {posts.map((post) => (
           <li key={post.id} className='list-group-item'>
@@ -25,6 +16,16 @@ const PostList = () => {
           </li>
         ))}
       </ul>
+      <button
+        disabled={page === 1}
+        onClick={() => setPage(page - 1)}
+        className='btn btn-primary my-3'
+      >
+        Previous
+      </button>
+      <button onClick={() => setPage(page + 1)} className='btn btn-primary my-3 ms-1'>
+        Next
+      </button>
     </>
   );
 };
