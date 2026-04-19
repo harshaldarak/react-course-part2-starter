ec4d51246de2423ab3de16f3dc74a07d9827b7f6
Harshal Darak <harshalddarak09@gmail.com>
2026-04-19 16:12:04 +0530

    feat: Refactor todo service and hooks to improve code organization and maintainability

--- src/react-query/hooks/useAddTodo.ts
+++ src/react-query/hooks/useAddTodo.ts
@@ -1,9 +1,7 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { CACHE_KEY_TODOS } from "../constants";
-import APIClient from "../services/apiClient";
-import { Todo } from "./useTodos";
+import todoService, { Todo } from "../services/todoService";
 
-const apiClient = new APIClient<Todos>("/todos");
 interface AddTodoContext {
   previousTodos: Todo[];
 }
@@ -11,7 +9,7 @@ interface AddTodoContext {
 const useAddTod = (onAdd: () => void) => {
   const queryClient = useQueryClient();
   return useMutation<Todo, Error, Todo, AddTodoContext>({
-    mutationFn: apiClient.post,
+    mutationFn: todoService.post,
     onMutate: (newTodo: Todo) => {
       const previousTodos =
         queryClient.getQueryData<Todo[]>(CACHE_KEY_TODOS) || [];
