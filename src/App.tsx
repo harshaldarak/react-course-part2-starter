9f525a12c6e4209c893a6e8dee27dee59bccfebd
Harshal Darak <harshalddarak09@gmail.com>
2026-05-12 00:09:40 +0530

    feat: Create AuthProvider component for authentication context and refactor App component to use it

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
 import { useReducer } from "react";
 import "./App.css";
 
-import AuthContext from "./state-management/contexts/authContext";
+import AuthProvider from "./state-management/AuthProvider";
 import TasksContext from "./state-management/contexts/taskContext";
 import HomePage from "./state-management/HomePage";
 import NavBar from "./state-management/NavBar";
-import authReducer from "./state-management/reducers/authReducer";
 import taskReducer from "./state-management/reducers/taskReducer";
 
 function App() {
   const [tasks, tasksDispatch] = useReducer(taskReducer, []);
-  const [user, authDispatch] = useReducer(authReducer, "");
 
   return (
-    <AuthContext.Provider value={{ user, dispatch: authDispatch }}>
+    <AuthProvider>
       <TasksContext.Provider value={{ tasks, dispatch: tasksDispatch }}>
         <NavBar />
         <HomePage />
       </TasksContext.Provider>
-    </AuthContext.Provider>
+    </AuthProvider>
   );
 }
 
