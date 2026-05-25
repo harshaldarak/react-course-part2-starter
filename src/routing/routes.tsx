1c7e2ed8f478d9ddf927a63d75ae1e9e8a498982
Harshal Darak <harshalddarak09@gmail.com>
2026-05-26 00:42:46 +0530

    feat: Update routing structure to include nested routes and enhance NavBar with Link components

--- src/routing/routes.tsx
+++ src/routing/routes.tsx
@@ -1,25 +1,32 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom";
 import ContactPage from "./ContactPage";
 import HomePage from "./HomePage";
-import UserListPage from "./UserListPage";
+import Layout from "./Layout";
 import UserDetailPage from "./UserDetailPage";
+import UserListPage from "./UserListPage";
 
 const route = createBrowserRouter([
   {
     path: "/",
-    element: <HomePage />,
-  },
-  {
-    path: "/users",
-    element: <UserListPage />,
-  },
-  {
-    path: "/users/:id",
-    element: <UserDetailPage />,
-  },
-  {
-    path: "/contact",
-    element: <ContactPage />,
+    element: <Layout />,
+    children: [
+      {
+        index :  true,
+        element: <HomePage />,
+      },
+      {
+        path: "users",
+        element: <UserListPage />,
+      },
+      {
+        path: "users/:id",
+        element: <UserDetailPage />,
+      },
+      {
+        path: "contact",
+        element: <ContactPage />,
+      },
+    ],
   },
 ]);
 
