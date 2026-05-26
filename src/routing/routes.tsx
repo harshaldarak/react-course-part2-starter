34938b201a968f4f7b3791936dee4b68f5225511
Harshal Darak <harshalddarak09@gmail.com>
2026-05-27 00:39:36 +0530

    feat: Implement custom ErrorPage component for improved error handling in routing

--- src/routing/routes.tsx
+++ src/routing/routes.tsx
@@ -1,4 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
+import ErrorPage from "./ErrorPage";
 import HomePage from "./HomePage";
 import Layout from "./Layout";
 import UserDetail from "./UserDetail";
@@ -8,6 +9,7 @@ const route = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
+    errorElement: <ErrorPage />,
     children: [
       { index: true, element: <HomePage /> },
       {
