c8307823f98ecf15f61df86b94b2aab793ea64d4
Harshal Darak <harshalddarak09@gmail.com>
2026-05-27 00:00:33 +0530

    refactor: Exercise nested routes

--- src/routing/UserDetail.tsx
+++ src/routing/UserDetail.tsx
@@ -1,13 +1,13 @@
 import { useLocation, useParams, useSearchParams } from "react-router-dom";
 
-const UserDetailPage = () => {
+const UserDetail = () => {
   const params = useParams();
-  // console.log(params);
+  console.log(params);
   const [searchParams, setSearchParams] = useSearchParams();
   // console.log(searchParams.get("name"));
   const location = useLocation();
-  // console.log(location);
-  return <p>User</p>;
+  // console.log(location); 
+  return <p>User {params.id}</p>;
 };
 
-export default UserDetailPage;
+export default UserDetail;
--- src/routing/routes.tsx
+++ src/routing/routes.tsx
@@ -1,30 +1,19 @@
-import { createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom";
-import ContactPage from "./ContactPage";
+import { createBrowserRouter } from "react-router-dom";
 import HomePage from "./HomePage";
 import Layout from "./Layout";
-import UserDetailPage from "./UserDetailPage";
-import UserListPage from "./UserListPage";
+import UserDetail from "./UserDetail";
+import UsersPage from "./UsersPage";
 
 const route = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
     children: [
-      {
-        index :  true,
-        element: <HomePage />,
-      },
+      { index: true, element: <HomePage /> },
       {
         path: "users",
-        element: <UserListPage />,
-      },
-      {
-        path: "users/:id",
-        element: <UserDetailPage />,
-      },
-      {
-        path: "contact",
-        element: <ContactPage />,
+        element: <UsersPage />,
+        children: [{ path: ":id", element: <UserDetail /> }],
       },
     ],
   },
